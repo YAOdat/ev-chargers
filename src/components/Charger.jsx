c8222f3299cbc68be4840be2fc6ae04940106471
Yaser Odat <odatuae@gmail.com>
2023-05-27 20:22:06 +0000

    added product page

--- src/components/Charger.jsx
+++ src/components/Charger.jsx
@@ -1,45 +1,80 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import chargerData from "../assets/chargers.json";
+import { FaWhatsapp } from "react-icons/fa";
+import { ImBackward } from "react-icons/im";
 import "../style/charger.css";
 
 function Charger() {
   const { chargerId } = useParams();
 
   // Find the charger with the matching id
-  const charger = chargerData.find((charger) => charger.id === chargerId);
+  const charger = chargerData.find(
+    (charger) => charger.id === parseInt(chargerId)
+  );
 
   if (!charger) {
     return <div>Charger not found.</div>;
   }
 
-  const { name, price, description, image, category, relatedProducts } = charger;
+  const { name, price, image, description, relatedProducts } = charger;
 
   return (
-    <div className="charger-container">
-      <div className="charger-image">
+    <div className="one-charger-container">
+      <div className="back-button-container">
+        <button className="back-button">
+          <span className="back-icon">
+            <ImBackward className="back-icon" />
+          </span>
+          <Link className="back-link" to="/chargers">
+            Back to Chargers
+          </Link>
+        </button>
+      </div>
+
+      <div className="one-charger-image">
         <img src={image} alt={name} />
       </div>
-      <div className="charger-details">
-        <h2 className="charger-name">{name}</h2>
-        <p className="charger-price">Price: {price}</p>
-        <p className="charger-description">{description}</p>
+      <div className="one-charger-details">
+        <h2 className="one-charger-name">{name}</h2>
+        <p className="one-charger-price">Price: {price}</p>
+        <p className="one-charger-description">{description}</p>
       </div>
+      <a
+        href={`https://api.whatsapp.com/send?phone=971501679410&text=mevcharger.com-${encodeURIComponent(
+          charger.name
+        )}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="order-button"
+      >
+        <FaWhatsapp className="whatsapp-icon" />
+        Order on WhatsApp
+      </a>
+
       <div className="related-products">
         <h3>Related Products</h3>
-        <div className="related-product-list">
-          {relatedProducts.map((productId) => {
-            const relatedProduct = chargerData.find(
-              (charger) => charger.id === productId
-            );
+        {relatedProducts.map((productId) => {
+          const relatedProduct = chargerData.find(
+            (product) => product.id === productId
+          );
+          if (relatedProduct) {
             return (
-              <div key={relatedProduct.id} className="related-product-item">
-                <img src={relatedProduct.image} alt={relatedProduct.name} />
-                <p>{relatedProduct.name}</p>
+              <div key={relatedProduct.id} className="card">
+                <img
+                  className="related-product-image"
+                  src={relatedProduct.image}
+                  alt={relatedProduct.name}
+                />
+                <h3 className="related-product-title">{relatedProduct.name}</h3>
+                <p className="related-product-description">
+                  {relatedProduct.description}
+                </p>
               </div>
             );
-          })}
-        </div>
+          }
+          return null;
+        })}
       </div>
     </div>
   );
